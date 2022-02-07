798d7248db9ba91225763f7ede9dae6aeedaea47
Aysan Isayo <aisayo@icloud.com>
2022-02-07 16:13:00 -0500

    lecture solution

--- 06-dom-events/index.js
+++ 06-dom-events/index.js
@@ -1,3 +1,12 @@
+// events: are some interaction with html elements
+// target element, fire off some logic that is meant for that particular event
+
+// event types: "click", "submit", "mouseover", "keydown"
+// event listener: 
+// - .addEventListener: called on our targeted element, takes in 2 arguments: ("event type"), event handler(callback function)
+// event object: plain old js object that gets created after an event occurs, it gets pased into our callback function 
+// event handler: callback function that operates on the event reaction, second argument to .addeventlistener()
+
 const pokemon = [
   {
     id: 1,
@@ -32,12 +41,38 @@ const pokemon = [
 ];
 
 const pokeContainer = document.querySelector("#poke-container");
+const pokeForm = document.querySelector('#poke-form')
+
+// targeting pokeForm and attaching event listener in the global scope 
+pokeForm.addEventListener('submit', function(event){
+
+  // preventing the forms default behavior
+  event.preventDefault()
+  
+  // grab the values from the form that was submitted:
+  const name = document.querySelector('#name-input').value
+  const img = document.querySelector('#img-input').value
+
+  // create a new pokemon object 
+  let newChar = {
+    id: 6, // NEEDS TO CHANGE,
+    name: name,
+    img: img,
+    likes: 0
+  }
+
+  // need to create a card for the new pokemon and put it on the DOM
+  renderPokemon(newChar)
+
+  // reset the form to empty fields 
+  pokeForm.reset()
+})
 
 pokemon.forEach(function (char) {
   renderPokemon(char);
 });
 
-function renderPokemon(pokemon) {
+function renderPokemon(pokemon) { // accepting a character obj with properties
   const pokeCard = document.createElement("div");
   pokeCard.id = `poke-${pokemon.id}`;
   pokeCard.className = "poke-card";
@@ -49,6 +84,55 @@ function renderPokemon(pokemon) {
   const pokeName = document.createElement("h3");
   pokeName.textContent = pokemon.name;
 
-  pokeCard.append(pokeImg, pokeName);
+  // creating likes num + button 
+  const pokeLikes = document.createElement("h3")
+  pokeLikes.textContent = "Likes: "
+
+  const likeNum = document.createElement("h3")
+  likeNum.className = "likes-num"
+  likeNum.textContent = pokemon.likes
+
+  const likesBttn = document.createElement("button")
+  likesBttn.className = "like-bttn"
+  likesBttn.textContent = "♥"
+
+  // attaching a click event to the likes bttn 
+  likesBttn.addEventListener("click", function(){
+      // increment the pokemon likes by 1 
+      // use character that event was invoked on, to increment number of likes
+      ++pokemon.likes
+
+      // update the text on the dom to reflect the new number 
+      // do we have access already to the element that holds the num?
+      // how can we use that?
+      likeNum.textContent = pokemon.likes
+
+  })
+
+  const deleteBttn = document.createElement("button")
+  deleteBttn.className = "delete-bttn"
+  deleteBttn.textContent = "delete"
+
+  // attaching a click event to the deleteBttn, passing in an event handler 
+  // deleteBttn.addEventListener("click", handleDelete)
+  // deleteBttn.addEventListener("click", function () {
+  //   pokeCard.remove();
+  // });
+
+  deleteBttn.addEventListener('click', () => handleDelete(pokeCard))
+  
+  pokeCard.append(pokeImg, pokeName, pokeLikes, likeNum, likesBttn, deleteBttn);
   pokeContainer.appendChild(pokeCard);
+}
+
+// function handleDelete(event){
+//   // event object
+//   // we're targeting the element that fired off the event
+//   // moving up the dom tree to the parent of that element
+//   // removing that element using .remove()
+//   event.target.parentNode.remove()
+// }
+
+function handleDelete(pokeCard){
+  pokeCard.remove()
 }
